58d0688857094de6f46f8831b67ec7dee2f43629
Jai Mangos <jaimangos@gmail.com>
2020-03-04 12:28:21 +1100

    Cleaned up code

--- model.py
+++ model.py
@@ -1,20 +1,18 @@
 import csv
 from pynode.main import *
 
-f = open("pipes.csv","r")
-data = list(csv.reader(f))
-f.close()
+with open("pipes.csv","r") as f:
+    data = list(csv.reader(f))[1:]
 
-header = data[0]
-data = data[1:]
+header = ['Node 1', 'Node 2', 'length', 'diameter', 'material', 'year', \
+'elasticity', 'wall_thickness', 'poissions_ratio', 'Kr', 'wave_speed']
 
-print(header)
 def run():
     for i in range(15):
         graph.add_node(str(i+1))
 
     for row in data:
-        edge = Edge(row[0],row[1])
+        edge = Edge(row[0],row[1],weight=row[-1]+'m/s')
         for i in range(len(row)-2):
             edge.set_attribute(header[i],row[i])
         graph.add_edge(edge)
