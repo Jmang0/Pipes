22969b39c894428a4363cb276c43f2f3c20102eb
Jai Mangos <jaimangos@gmail.com>
2020-03-11 12:08:00 +1100

    Added the sensor attribute (and styling)

--- model.py
+++ model.py
@@ -8,13 +8,31 @@ header = ['Node 1', 'Node 2', 'length', 'diameter', 'material', 'year', \
 'elasticity', 'wall_thickness', 'poissions_ratio', 'Kr', 'wave_speed']
 
 def run():
+    # Add nodes
     for i in range(15):
-        graph.add_node(str(i+1))
+        node = Node(str(i+1))
+        node.set_attribute('sensor',False)
+        graph.add_node(node)
 
+    # Add edges
     for row in data:
         edge = Edge(row[0],row[1],weight=row[-1]+'m/s')
         for i in range(len(row)-2):
             edge.set_attribute(header[i],row[i])
         graph.add_edge(edge)
 
+    graph.add_node('text')
+
+
+def add_sensor(node):
+    if node.attribute('sensor'):
+        node.set_attribute('sensor',False)
+        node.set_color(Color.DARK_GREY)
+
+    else:
+        node.set_attribute('sensor',True)
+        node.set_color(Color.GREEN)
+
 begin_pynode(run)
+
+register_click_listener(add_sensor)
